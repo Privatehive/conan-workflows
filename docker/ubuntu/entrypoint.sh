1c0ca69a2002587d106e0451e37f1f693848ed8b
Björn Stresing <bjoern.stresing@gmx.de>
2024-08-18 00:08:49 +0200

    Enable tools.system.package_manager:mode = install and tools.system.package_manager:sudo by default in the conan-ubunto Docker image

--- docker/ubuntu/entrypoint.sh
+++ docker/ubuntu/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 TMPFILE=$(mktemp /tmp/cmd-XXXXXXXX.sh)
-echo "#!/bin/bash\nsudo apt-get -y -q update\nconan profile detect -vquiet\n$@" > $TMPFILE
+echo "#!/bin/bash\nsudo apt-get -y -q update\nconan profile detect -vquiet\necho 'tools.system.package_manager:mode = install' >> /home/conan/.conan/global.conf\necho 'tools.system.package_manager:sudo = True' >> /home/conan/.conan/global.conf\n$@" > $TMPFILE
 exec bash -e $TMPFILE
