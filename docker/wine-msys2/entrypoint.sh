3143a1aa05d78ece86d72b00a2f2352854de270d
Björn Stresing <bjoern.stresing@gmx.de>
2025-05-24 00:02:05 +0200

    Add GitHub package registry cleanup step to workflow

--- docker/wine-msys2/entrypoint.sh
+++ docker/wine-msys2/entrypoint.sh
@@ -2,5 +2,5 @@
 
 TMPFILE=$(mktemp /tmp/cmd-XXXXXXXX.bat)
 printf "@echo off\r\nset Python_ROOT_DIR=C:\\Python\r\nconan profile detect -vquiet\r\n$@" > $TMPFILE
-. /opt/mkuserwineprefix
-WINEPATH="c:/msys64/usr/bin" exec wine64 cmd /S /C $TMPFILE
+#. /opt/mkuserwineprefix
+exec wine64 cmd /S /C $TMPFILE
