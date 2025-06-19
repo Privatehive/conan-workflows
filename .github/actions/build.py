f0c6afd9a0a3dbb6b19c24eaa19f9714528ba26a
Björn Stresing <bjoern.stresing@gmx.de>
2025-06-19 23:15:33 +0200

    Introduce 'conan_build_profile', 'conan_host_profile_path', 'conan_build_profile_path'

--- .github/actions/build.py
+++ .github/actions/build.py
@@ -17,12 +17,20 @@ helper_dir = os.path.dirname(__file__)
 if __name__ == "__main__":
 
     host_profile_path = None
+    build_profile_path = None
     check_call("conan profile detect -f", shell=True)
     check_call("conan config install %s" % os.path.join(helper_dir, "global.conf"), shell=True)
     check_call("conan config install -tf extensions/hooks %s" % os.path.join(helper_dir, "hook_clean_cache.py"), shell=True)
 
+    options = ""
+
     if 'CONAN_HOST_PROFILE_PATH' in os.environ and os.environ['CONAN_HOST_PROFILE_PATH']:
         host_profile_path = os.environ['CONAN_HOST_PROFILE_PATH']
+        options += " -pr:h %s " % host_profile_path
+
+    if 'CONAN_BUILD_PROFILE_PATH' in os.environ and os.environ['CONAN_BUILD_PROFILE_PATH']:
+        build_profile_path = os.environ['CONAN_BUILD_PROFILE_PATH']
+        options += " -pr:b %s " % build_profile_path
 
     recipe_path = "./"
       
@@ -36,14 +44,13 @@ if __name__ == "__main__":
                 print("Adding remote: " + rep_name + " url: " + remote)
                 check_call("conan remote add --index 0 --force %s %s" % (rep_name, remote), shell=True)
 
-    options = ""
     if 'CONAN_OPTIONS' in os.environ and os.environ['CONAN_OPTIONS']:
         for option in os.environ['CONAN_OPTIONS'].split(','):
             options += " -o " + option
 
-    if 'CONAN_BUILD_REQUIRE' in os.environ and host_profile_path and os.environ['CONAN_BUILD_REQUIRE'] == "true":
+    if 'CONAN_BUILD_REQUIRE' in os.environ and os.environ['CONAN_BUILD_REQUIRE'] == "true":
         print("build-require option is used")
-        options += " --build-require"
+        options += " --build-require "
 
     deploy_path = None
     if 'CONAN_DEPLOY_PATH' in os.environ and os.environ['CONAN_DEPLOY_PATH']:
@@ -56,15 +63,9 @@ if __name__ == "__main__":
 
     package_ref = "%s/%s@%s/%s" % (name, version, user, cannel)
 
-    if host_profile_path is not None:
-        print("Cross building recipe: " + package_ref)
-        check_call("conan create %s -pr:h \"%s\" -tf \"\" %s -u -b missing" % (recipe_path, host_profile_path, options), shell=True)
-        if deploy_path is not None:
-            check_call("conan install --requires=\"%s\" -pr:h \"%s\" %s --deployer-package=\"%s/*\" --deployer-folder=\"%s\"" % (package_ref, host_profile_path, options, name, deploy_path), shell=True)
-    else:
-        print("Building recipe: " + package_ref)
-        check_call("conan create %s -tf \"\" %s -u -b missing" % (recipe_path, options), shell=True)
-        if deploy_path is not None:
-            check_call("conan install --requires=\"%s\" %s --deployer-package=\"%s/*\" --deployer-folder=\"%s\"" % (package_ref, options, name, deploy_path), shell=True)
+    print("Building recipe: " + package_ref)
+    check_call("conan create %s -tf \"\" %s -u -b missing" % (recipe_path, options), shell=True)
+    if deploy_path is not None:
+        check_call("conan install --requires=\"%s\" %s --deployer-package=\"%s/*\" --deployer-folder=\"%s\"" % (package_ref, options, name, deploy_path), shell=True)
 
     print("-----Finished-----")
