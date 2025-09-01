636256d249ff19bf8db22885da2695d9f9d86724
Björn Stresing <bjoern.stresing@gmx.de>
2025-09-01 20:20:49 +0200

    Print conan errors

--- .github/actions/build.py
+++ .github/actions/build.py
@@ -69,17 +69,17 @@ if __name__ == "__main__":
     user = json.loads(check_output("conan inspect %s -f json" % recipe_path, shell=True).decode("ascii"))["user"]
     cannel = json.loads(check_output("conan inspect %s -f json" % recipe_path, shell=True).decode("ascii"))["channel"]
 
-    for key, val in json.loads(check_output("conan graph info %s -vquiet -f json" % recipe_path, shell=True).decode("ascii"))["graph"]["resolved_ranges"].items():
+    for key, val in json.loads(check_output("conan graph info %s -verror -f json" % recipe_path, shell=True).decode("ascii"))["graph"]["resolved_ranges"].items():
         if val.startswith("qt/"):
             print("found qt dependency - downloading recipe to search for conan profiles")
             for remote in remotes:
                 # download recipe containing conan profiles
                 with tempfile.TemporaryDirectory() as tmpdir:
                     try:
-                        check_output("conan download %s -vquiet -f json --only-recipe -cc core.cache:storage_path=%s -r %s" % (val, tmpdir, remote), shell=True)
+                        check_output("conan download %s -verror -f json --only-recipe -cc core.cache:storage_path=%s -r %s" % (val, tmpdir, remote), shell=True)
                     except Exception:
                         continue
-                    tmp_recipe_path = json.loads(check_output("conan cache path %s -vquiet -cc core.cache:storage_path=%s -f json" % (val, tmpdir), shell=True).decode("ascii"))["cache_path"]
+                    tmp_recipe_path = json.loads(check_output("conan cache path %s -verror -cc core.cache:storage_path=%s -f json" % (val, tmpdir), shell=True).decode("ascii"))["cache_path"]
                     tmp_profile_path = os.path.join(tmp_recipe_path, 'profiles')
                     print("found conan profiles in qt recipe - installing")
                     if os.path.isdir(tmp_profile_path):
